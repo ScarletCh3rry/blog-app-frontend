36dd580bcdb043ba399e3b268f25506c8eeb0ce3
79209 <IlyaBogom@gmail.com>
2022-06-16 03:04:47 +0300

    axios heroku commit

--- src/API/axios-instance.ts
+++ src/API/axios-instance.ts
@@ -3,7 +3,7 @@ import {ACCESS_TOKEN} from "../store/AuthStore";
 
 
 export const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: 'https://cherry-blog-backend.herokuapp.com/api/',
     headers: {
         'Content-Type': 'application/json'
     }
